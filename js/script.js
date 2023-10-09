1283ccd23744d8a5b1b4c829adbd2b3569e72397
davidelunetta <davidelunetta.27@gmail.com>
2023-10-09 13:18:01 +0200

    updated exercise

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let nome = prompt("Inserisci il tuo nome:");
+var cognome = prompt("Inserisci il tuo cognome:");
+
+var colore = prompt("Inserisci il tuo colore preferito:");
+var risultato = nome + cognome + colore + "21";
+
+document.write(risultato);
