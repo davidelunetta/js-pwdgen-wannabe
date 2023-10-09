eb47a365bdaafd4fd999ad2763dca9185b1b054a
davidelunetta <davidelunetta.27@gmail.com>
2023-10-09 13:32:06 +0200

    modifid project

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 let nome = prompt("Inserisci il tuo nome:");
-var cognome = prompt("Inserisci il tuo cognome:");
+let cognome = prompt("Inserisci il tuo cognome:");
 
-var colore = prompt("Inserisci il tuo colore preferito:");
-var risultato = nome + cognome + colore + "21";
+let colore = prompt("Inserisci il tuo colore preferito:");
+let risultato = nome + cognome + colore + "21";
 
 document.write(risultato);
